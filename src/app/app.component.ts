3d25a67f67079b2a1bb4289073a3bb7cb50ba93d
Dipesh Kumar <dipeshkrroy@gmail.com>
2021-01-17 15:35:25 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
+import { ReadKeyExpr } from '@angular/compiler';
 import { Component } from '@angular/core';
-
+import * as XLSX from 'xlsx';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'excelReader';
+  data:[][];
+  onFileUpload(evt:any){
+    const target : DataTransfer = <DataTransfer> (evt.target);
+
+    if(target.files.length !=1) throw new Error("Please upload single file at once");
+    const reader:FileReader = new FileReader();
+    reader.onload = (e:any) =>{
+      const bstr:string = e.target.result;
+
+      const wb: XLSX.WorkBook = XLSX.read( bstr,{type: 'binary'});
+      const wsname = wb.SheetNames[0];
+      const ws:XLSX.WorkSheet = wb.Sheets[wsname];
+      console.log(ws);
+      this.data =(XLSX.utils.sheet_to_json(ws,{header: 1}));
+      console.log(this.data)
+    }
+    reader.readAsBinaryString(target.files[0]);
+  }
 }
